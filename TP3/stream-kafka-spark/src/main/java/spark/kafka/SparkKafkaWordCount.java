6250bc3f6e89977e427df8a35914be9826b6674e
addalaraed <addala.raed@gmail.com>
2025-05-10 14:12:36 +0100

    simplified pom.xml

--- TP3/stream-kafka-spark/src/main/java/spark/kafka/SparkKafkaWordCount.java
+++ TP3/stream-kafka-spark/src/main/java/spark/kafka/SparkKafkaWordCount.java
@@ -13,41 +13,46 @@ import scala.Tuple2;
 
 public class SparkKafkaWordCount {
 
-    public static void main(String[] args) throws Exception {
+        public static void main(String[] args) throws Exception {
 
-        if (args.length < 3) {
-            System.err.println("Usage: SparkKafkaWordCount <bootstrap-servers> <subscribe-topics> <group-id>");
-            System.exit(1);
+                if (args.length < 3) {
+                        System.err.println(
+                                        "Usage: SparkKafkaWordCount <bootstrap-servers> <subscribe-topics> <group-id>");
+                        System.exit(1);
+                }
+
+                String bootstrapServers = args[0];
+                String topics = args[1];
+                String groupId = args[2];
+
+                SparkSession spark = SparkSession
+                                .builder()
+                                .appName("SparkKafkaWordCount")
+                                .getOrCreate();
+
+                // Set Spark log level to WARN as a fallback
+                spark.sparkContext().setLogLevel("WARN");
+
+                // Create DataFrame representing the stream of input lines from Kafka
+                Dataset<Row> df = spark
+                                .readStream()
+                                .format("kafka")
+                                .option("kafka.bootstrap.servers", bootstrapServers)
+                                .option("subscribe", topics)
+                                .option("kafka.group.id", groupId)
+                                .load();
+
+                df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+                                .as(Encoders.tuple(Encoders.STRING(), Encoders.STRING()))
+                                .flatMap((FlatMapFunction<Tuple2<String, String>, String>) value -> Arrays
+                                                .asList(value._2.split(" "))
+                                                .iterator(), Encoders.STRING())
+                                .groupByKey((MapFunction<String, String>) value -> value, Encoders.STRING())
+                                .count()
+                                .writeStream()
+                                .outputMode("complete")
+                                .format("console")
+                                .start()
+                                .awaitTermination();
         }
-
-        String bootstrapServers = args[0];
-        String topics = args[1];
-        String groupId = args[2];
-
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("SparkKafkaWordCount")
-                .getOrCreate();
-
-        // Create DataFrame representing the stream of input lines from Kafka
-        Dataset<Row> df = spark
-                .readStream()
-                .format("kafka")
-                .option("kafka.bootstrap.servers", bootstrapServers)
-                .option("subscribe", topics)
-                .option("kafka.group.id", groupId)
-                .load();
-
-        df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-                .as(Encoders.tuple(Encoders.STRING(), Encoders.STRING()))
-                .flatMap((FlatMapFunction<Tuple2<String, String>, String>) value -> Arrays.asList(value._2.split(" "))
-                        .iterator(), Encoders.STRING())
-                .groupByKey((MapFunction<String, String>) value -> value, Encoders.STRING())
-                .count()
-                .writeStream()
-                .outputMode("complete")
-                .format("console")
-                .start()
-                .awaitTermination();
-    }
 }
